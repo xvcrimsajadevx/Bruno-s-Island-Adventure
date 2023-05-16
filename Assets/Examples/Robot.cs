0290a8b543ad4f515ba2d2cd72bf9ae398bf3422
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-05-15 20:01:01 -0600

    Robot/Section 4, Action Map, Player Object Placeholder created

---
+++ Assets/Examples/Robot.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+
+// RPG is the main namespace
+// for our game
+namespace RPG.Example
+{
+    public class Robot : MonoBehaviour
+    {
+        /*
+        Variables
+        =========
+        */
+
+        /*
+        public int age = 5;
+        public float price = 99.99f;
+        // private string name = "McBot";
+        public bool isTurnedOn = false;
+
+        public Robot()
+        {
+            // isTurnedOn = true;
+            float newPrice = CalculatePrice(0.7f, 1);
+
+            if (newPrice > 75f)
+            {
+                price = newPrice;
+            }
+            else
+            {
+                // print("Price is too low.");
+                // Debug.Log("Price is too low.");
+                // Debug.LogWarning("Price is too low.");
+                // Debug.LogError("Price is too low.");
+
+                Log("Price is too low!");
+                Log<int>(age);
+                Log(isTurnedOn);
+            }
+        }
+
+        [Obsolete("CalculatePrice() is deprecated. Use ApplyDiscount() instead.")]
+        public float CalculatePrice(float discount, int quantity)
+        {   
+            return (price - (price * discount)) * quantity;
+        }
+
+        public float ApplyDiscount(float discount)
+        {
+            return (price - (price * discount));
+        }
+
+        public void Log<T>(T message)
+        {
+            Debug.Log(message);
+        }
+        */
+    }
+}
